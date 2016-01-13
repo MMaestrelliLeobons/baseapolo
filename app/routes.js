bf860b5b6b83a6073200fd99071ca3b0cc6b850d
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-01-13 09:31:51 -0200

    Improving structure of the backend.

---
+++ app/routes.js
@@ -0,0 +1,6 @@
+/**
+ *
+ */
+
+var baseapolo = require('./models/baseapolo');
+
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var express = require('express'),
-	http = require('http');
+/**
+ *
+ */
 
-var app = express();
+// modules ==========================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var	methodOverride = require('method-override');
+var	http = require('http');
 var path = require('path');
 
+var app = express();
+
+// Configuration ====================================================
 app.set('port', process.env.PORT || 3000);
+
+var db = require('./config/db');
+//mongoose.connect(db.url)
+
+// Routes ===========================================================
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Listen ===========================================================
 http.createServer(app).listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 });
