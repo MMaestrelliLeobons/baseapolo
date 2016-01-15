225d4a736cb573c9e9850a56bfed8a06cd59e0d7
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-01-15 10:59:48 -0200

    Small changes in the front-end. Restructuring to grow.

--- app/models/baseapolo.js
+++ app/models/baseapolo.js
@@ -4,6 +4,15 @@
 
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('baseapolo', {
+module.exports = mongoose.model('opportunity', {
+	title: String,
+	post_date: Date,
+	end_date: Date,
+	//admin: {user.id : user.name},
+	likes: int,
+	description: String
+});
 
+module.exports = mongoose.model('user', {
+	name: String
 });
