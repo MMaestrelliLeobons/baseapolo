cccc7568145c45097f9737c7ea4c2dabbdb8696b
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-01-17 00:54:10 -0200

    Finishing the refactoring

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@
  
 module.exports = {
 	'port': process.env.PORT || 3000,
-    'db_url' : process.env.DB_HOST,
-    'secret': process.env.SECRET
+	'db_url' : process.env.DB_HOST,
+	'secret': process.env.SECRET
 };
